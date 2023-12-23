c4b1940c15721f7db5adaccfe823492e9599366e
BowTiedDevil <devil@bowtieddevil.com>
2023-12-23 11:58:22 -0800

    Add Curve pool update methods, tests

--- src/degenbot/curve/curve_stableswap_dataclasses.py
+++ src/degenbot/curve/curve_stableswap_dataclasses.py
@@ -1,5 +1,7 @@
 import dataclasses
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Optional
+
+from eth_typing import ChecksumAddress
 
 if TYPE_CHECKING:
     from .curve_stableswap_liquidity_pool import CurveStableswapPool
@@ -17,3 +19,14 @@ class CurveStableswapPoolSimulationResult:
     amount1_delta: int
     current_state: CurveStableswapPoolState
     future_state: CurveStableswapPoolState
+
+
+@dataclasses.dataclass(slots=True, eq=False)
+class CurveStableswapPoolExternalUpdate:
+    block_number: int = dataclasses.field(compare=False)
+    sold_id: int
+    bought_id: int
+    tokens_sold: int
+    tokens_bought: int
+    buyer: Optional[ChecksumAddress] = dataclasses.field(default=None)
+    tx: Optional[str] = dataclasses.field(compare=False, default=None)
